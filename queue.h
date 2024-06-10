e140358360255ba520c0ada38afc00b9bb2d74ab
WiktorStefaniak <166062137+WiktorStefaniak@users.noreply.github.com>
2024-06-10 17:42:44 +0200

    stos

---
+++ queue.h
@@ -0,0 +1,14 @@
+//
+// Created by wiktor on 31.05.2024.
+//
+
+#ifndef STOS_KOLEJKA_QUEUE_H
+#define STOS_KOLEJKA_QUEUE_H
+
+int isQueueEmpty(void);
+int isQueueFull(void);
+int remove(void);
+int first(void);
+void add(int val);
+
+#endif //STOS_KOLEJKA_QUEUE_H
